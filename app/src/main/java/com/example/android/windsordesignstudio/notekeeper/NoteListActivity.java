2c2f50fa485857f7bb4b55fb7f55f61332d771f5
Rockwellwindsor <rockwellwindsor@gmail.com>
2018-02-20 10:11:11 -0600

    Development: Adds cardview for the RecyclerView item
    Imports ibrary to Gradle file and styles the xml layout file
    
    Changes to be committed:
    	modified:   app/build.gradle
    	modified:   app/src/main/java/com/example/android/windsordesignstudio/notekeeper/NoteListActivity.java
    	modified:   app/src/main/res/layout/content_note_list.xml
    	new file:   app/src/main/res/layout/item_note_list.xml

--- app/src/main/java/com/example/android/windsordesignstudio/notekeeper/NoteListActivity.java
+++ app/src/main/java/com/example/android/windsordesignstudio/notekeeper/NoteListActivity.java
@@ -4,17 +4,18 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 
 import java.util.List;
 
 public class NoteListActivity extends AppCompatActivity {
-
-    private ArrayAdapter<NoteInfo> mAdapterNotes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,32 +38,36 @@ public class NoteListActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
-        mAdapterNotes.notifyDataSetChanged();
+//        mAdapterNotes.notifyDataSetChanged();
     }
 
     private void initializeDisplayContent() {
 
-        // Get reference to ListView
-        final ListView mListNotes = (ListView) findViewById(R.id.list_notes);
+//        // Get reference to ListView
+//        final ListView mListNotes = (ListView) findViewById(R.id.list_notes);
+//
+//        // Get notes
+//        List<NoteInfo> notes = DataManager.getInstance().getNotes();
+//
+//        // Create Array Adapter
+//        mAdapterNotes = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, notes);
+//
+//        // Set Adapter to the note list
+//        mListNotes.setAdapter(mAdapterNotes);
+//
+//        // Display note on item click
+//        mListNotes.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+//                Intent intent = new Intent(NoteListActivity.this, NoteActivity.class);
+////                NoteInfo note = (NoteInfo) mListNotes.getItemAtPosition(position);
+//                intent.putExtra(NoteActivity.NOTE_POSITION, position);
+//                startActivity(intent);
+//            }
+//        });
 
-        // Get notes
-        List<NoteInfo> notes = DataManager.getInstance().getNotes();
-
-        // Create Array Adapter
-        mAdapterNotes = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, notes);
-
-        // Set Adapter to the note list
-        mListNotes.setAdapter(mAdapterNotes);
-
-        // Display note on item click
-        mListNotes.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Intent intent = new Intent(NoteListActivity.this, NoteActivity.class);
-//                NoteInfo note = (NoteInfo) mListNotes.getItemAtPosition(position);
-                intent.putExtra(NoteActivity.NOTE_POSITION, position);
-                startActivity(intent);
-            }
-        });
+         final RecyclerView recyclerNotes = (RecyclerView) findViewById(R.id.rv_list_notes);
+         final LinearLayoutManager notesLayoutManager = new LinearLayoutManager(this);
+         recyclerNotes.setLayoutManager(notesLayoutManager);
     }
 }
