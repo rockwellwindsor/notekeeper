51880a5c376c4412a9868f4a9f327f0ef2b1aa57
Rockwellwindsor <rockwellwindsor@gmail.com>
2018-03-27 20:55:59 -0500

    Development: Finished "Saving changes to a Note"
    Creates update functionality for notes.
    
    Changes to be committed:
    	modified:   app/src/main/java/com/example/android/windsordesignstudio/notekeeper/NoteActivity.java

--- app/src/main/java/com/example/android/windsordesignstudio/notekeeper/NoteActivity.java
+++ app/src/main/java/com/example/android/windsordesignstudio/notekeeper/NoteActivity.java
@@ -2,6 +2,7 @@ package com.example.android.windsordesignstudio.notekeeper;
 
 import android.annotation.SuppressLint;
 import android.app.LoaderManager;
+import android.content.ContentValues;
 import android.content.Intent;
 import android.content.Loader;
 import android.content.CursorLoader;
@@ -173,9 +174,33 @@ public class NoteActivity extends AppCompatActivity implements LoaderManager.Loa
     }
 
     private void saveNote() {
-//        mNote.setCourse((CourseInfo) mSpinnerCourses.getSelectedItem());
-//        mNote.setTitle(mTextNoteTitle.getText().toString());
-//        mNote.setText(mTextNoteText.getText().toString());
+        String courseId = selectedCourseId();
+        String noteTitle = mTextNoteTitle.getText().toString();
+        String noteText = mTextNoteText.getText().toString();
+        saveNoteToDatabase(courseId, noteTitle, noteText);
+    }
+
+    private String selectedCourseId() {
+        int selectedPosition = mSpinnerCourses.getSelectedItemPosition();
+        Cursor cursor = mAdapterCourses.getCursor();
+        cursor.moveToPosition(selectedPosition);
+        int courseIdPos = cursor.getColumnIndex(CourseInfoEntry.COLUMN_COURSE_ID);
+        String courseId = cursor.getString(courseIdPos);
+        return courseId;
+    }
+
+    private void saveNoteToDatabase(String courseId, String noteTitle, String noteText) {
+        String selection = NoteInfoEntry._ID + " = ?";
+        String[] selectionArgs = {Integer.toString(mNoteId)};
+
+        ContentValues values = new ContentValues();
+        values.put(NoteInfoEntry.COLUMN_COURSE_ID, courseId);
+        values.put(NoteInfoEntry.COLUMN_NOTE_TITLE, noteTitle);
+        values.put(NoteInfoEntry.COLUMN_NOTE_TEXT, noteText);
+
+        // Get connection to Database
+        SQLiteDatabase db = mDbOpenHelper.getWritableDatabase();
+        db.update(NoteInfoEntry.TABLE_NAME, values, selection, selectionArgs);
     }
 
     private void displayNote() {
